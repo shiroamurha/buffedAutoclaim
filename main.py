89f718db29912db8e216a04b11745b9cf2ae657e
Shiro Amurha <46139101+shiroamurha@users.noreply.github.com>
2022-02-11 18:43:39 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
-from selenium import webdriver
+import requests
+from requests.auth import HTTPBasicAuth
+from time import sleep
+
+#auth = HTTPBasicAuth('alayouey@gmail.com', 'shiro123', 'GDmzexCFXz')
+request = requests.Session()
 
 
-driver = webdriver.Opera()
+
+obj = request.request(
+    'get', 
+    'https://firefaucet.win/start',
+    cookies={
+        'session': 'e59670da-60ae-4559-a555-12f533cd317c.0vtn4o3qMik_cK3hJliW2uKCQUM'
+    }
+)
+
+html = open('request_html.html', 'w')
+#html.write(f'{obj.text}')
+html.close()
+print(obj)
+
+
+#
+#
+# print(request.headers)
+
+
+
+
+
+
+ # cookies='e59670da-60ae-4559-a555-12f533cd317c.0vtn4o3qMik_cK3hJliW2uKCQUM'
